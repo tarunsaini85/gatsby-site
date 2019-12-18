6041d452df029e828b96c1fd2977bbd3191ae77e
Tarunjeet Saini <tarunjeetsaini@197NODMB22226.local>
2019-12-18 10:34:30 +0530

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,9 +36,9 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()},
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://paytm.com">Paytm.com</a>
         </footer>
       </div>
     </>
